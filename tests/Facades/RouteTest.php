06f6447fe3c39d55e055d1b3b37d9a0059568c0b
Katarn <katarn@katarn.es>
2021-09-29 23:01:42 +0200

    Providers on param tests

--- tests/Facades/RouteTest.php
+++ tests/Facades/RouteTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Facades;
 
 use Chico\Facades\Route;
+use Generator;
 use Tests\TestCase;
 
 class RouteTest extends TestCase
@@ -51,34 +52,52 @@ class RouteTest extends TestCase
 
     public function test_it_should_pass_associated_params_by_name_to_the_action(): void
     {
-        $_SERVER['REQUEST_URI'] = 'https://example.org/foo/bar/bazz';
+        $_SERVER['REQUEST_URI'] = 'https://example.org/foo/bar/buzz';
         $_SERVER['REQUEST_METHOD'] = 'GET';
 
-        $this->expectOutputString("The params are 'bar', 'foo' and 'bazz'!");
+        $this->expectOutputString("The params are 'bar', 'foo' and 'buzz'!");
 
         Route::get('{secondParam}/{firstParam}/{thirdParam}', StubController::class, 'manyParamsAction');
     }
 
-    public function test_it_should_pass_string_params_to_the_action(): void
+    /** @dataProvider stringProvider */
+    public function test_it_should_pass_string_params_to_the_action(string $string): void
     {
-        $_SERVER['REQUEST_URI'] = 'https://example.org/expected/string/is/awesome';
+        $_SERVER['REQUEST_URI'] = "https://example.org/expected/string/is/$string";
         $_SERVER['REQUEST_METHOD'] = 'GET';
 
-        $this->expectOutputString("The 'string' param is 'awesome'!");
+        $this->expectOutputString("The 'string' param is '$string'!");
 
         Route::get('expected/string/is/{param}', StubController::class, 'stringParamAction');
     }
 
-    public function test_it_should_pass_int_params_to_the_action(): void
+    public function stringProvider(): Generator
     {
-        $_SERVER['REQUEST_URI'] = 'https://example.org/expected/integer/is/84';
+        yield 'foo' => ['string' => 'foo'];
+        yield 'bar' => ['string' => 'bar'];
+        yield 'buzz' => ['string' => 'buzz'];
+    }
+
+    /** @dataProvider intProvider */
+    public function test_it_should_pass_int_params_to_the_action(string $int): void
+    {
+        $_SERVER['REQUEST_URI'] = "https://example.org/expected/integer/is/$int";
         $_SERVER['REQUEST_METHOD'] = 'GET';
 
-        $this->expectOutputString("The 'int' param is '84'!");
+        $this->expectOutputString("The 'int' param is '$int'!");
 
         Route::get('expected/integer/is/{param}', StubController::class, 'intParamAction');
     }
 
+    public function intProvider(): Generator
+    {
+        for ($try = 0; $try < 10; $try++) {
+            $randomInt = (string) mt_rand();
+            yield "#$randomInt" => ['int' => $randomInt];
+        }
+    }
+
+    /** @dataProvider floatProvider */
     public function test_it_should_pass_float_params_to_the_action(): void
     {
         $_SERVER['REQUEST_URI'] = 'https://example.org/expected/float/is/8.4';
@@ -89,13 +108,30 @@ class RouteTest extends TestCase
         Route::get('expected/float/is/{param}', StubController::class, 'floatParamAction');
     }
 
-    public function test_it_should_pass_bool_params_to_the_action(): void
+    public function floatProvider(): Generator
     {
-        $_SERVER['REQUEST_URI'] = 'https://example.org/expected/bool/is/true';
+        for ($try = 0; $try < 10; $try++) {
+            $randomFloat = (string) (mt_rand() / mt_rand(1, 133746));
+            yield "#$randomFloat" => ['float' => $randomFloat];
+        }
+    }
+
+    /** @dataProvider boolProvider */
+    public function test_it_should_pass_bool_params_to_the_action(string $given, string $expected): void
+    {
+        $_SERVER['REQUEST_URI'] = "https://example.org/expected/bool/is/$given";
         $_SERVER['REQUEST_METHOD'] = 'GET';
 
-        $this->expectOutputString("The 'bool' param is 'true'!");
+        $this->expectOutputString("The 'bool' param is '$expected'!");
 
         Route::get('expected/bool/is/{param}', StubController::class, 'boolParamAction');
+    }
+
+    public function boolProvider(): Generator
+    {
+        yield 'true' => ['given' => 'true', 'expected' => 'true'];
+        yield 'false' => ['given' => 'false', 'expected' => 'false'];
+        yield '1' => ['given' => '1', 'expected' => 'true'];
+        yield '0' => ['given' => '0', 'expected' => 'false'];
     }
 }
